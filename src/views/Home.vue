1350b7d7a9014d337c872387410d6c5ea7962281
GrVl-dev <vlgrv80@gmail.com>
2020-11-28 21:13:25 +0400

    clear template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,16 @@
 <template>
-  <el-container class="home">
-    <el-main>
-      Main
-    </el-main>
-    <el-aside
-      class="hidden-sm-and-down"
-    >
-      Aside 2
-    </el-aside>
-  </el-container>
+  <div>
+1
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home'
+
 }
 </script>
 
 <style lang="scss" scoped>
- .el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  width: 300px;
- }
+
 </style>
