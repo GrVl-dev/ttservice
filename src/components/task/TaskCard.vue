a3eb3d5d3799f1483a86dbd5b2b0f9d4770d9864
GrVl-dev <vlgrv80@gmail.com>
2020-12-11 03:48:23 +0400

    task card create

--- src/components/task/TaskCard.vue
+++ src/components/task/TaskCard.vue
@@ -4,12 +4,86 @@
     <v-card
       elevation="2"
       outlined
+      class="task-card d-flex flex-wrap"
     >
       <v-list-item two-line>
         <v-list-item-content>
-          <v-list-item-title>Тестовое оборудование</v-list-item-title>
-          <v-list-item-subtitle>Не работает что-то</v-list-item-subtitle>
+          <v-list-item-title>
+            {{ task.equipment.title }}
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ task.equipment.description }}</v-list-item-subtitle>
         </v-list-item-content>
+
+        <v-spacer></v-spacer>
+        <v-list-item-content >
+          <v-list-item-subtitle class="d-flex align-center">
+            <v-icon
+              small
+              class="pr-2"
+            >
+              mdi-clipboard-list-outline
+            </v-icon>
+            <div>O01222123</div>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle class="d-flex align-center">
+            <v-icon
+              small
+              class="pr-2"
+            >
+              mdi-briefcase-variant-outline
+            </v-icon>
+            <div>ТоргТехникаСервис</div>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle
+            style="color: green;"
+          >
+            Назначена
+          </v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-content>
+          <v-list-item-subtitle class="d-flex align-center">
+            <v-icon
+              small
+              class="pr-2"
+            >
+              mdi-archive-arrow-down-outline
+            </v-icon>
+            <div>11.12.2020 22:00</div>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle class="d-flex align-center">
+            <v-icon
+              small
+              class="pr-2"
+            >
+              mdi-briefcase-variant-outline
+            </v-icon>
+            <div>До крайнего срока 1 год</div>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle class="d-flex align-center">
+            <v-icon
+              small
+              class="pr-2"
+            >
+              mdi-account-circle-outline
+            </v-icon>
+            <div>Пупкин И.И.</div>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle class="d-flex align-center">
+            <v-icon
+              small
+              class="pr-2"
+            >
+              mdi-alarm
+            </v-icon>
+            <div>11.12.2021 22:00</div>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn icon>
+            <v-icon color="rgb(0, 183, 255)">mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-list-item-action>
       </v-list-item>
     </v-card>
   </v-container>
@@ -19,11 +93,23 @@
 <script>
 export default {
   data: () => ({
-
-  })
+    icons: [
+      
+    ]
+  }),
+  props: [
+    'task'
+  ]
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.task-card {
+  border-left: 4px solid rgb(0, 183, 255);
+  :hover {
+    background-color: rgb(223, 245, 253);
+    cursor: pointer;
+  }
+}
 
 </style>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -12,7 +12,7 @@
       </v-toolbar>
     </v-card>
     
-    <TaskCard v-for="i in 5" :key="i"/>
+    <TaskCard v-for="task in tasks" :key="task.id" :task="task"/>
   </v-container>
 </template>
 
@@ -21,7 +21,54 @@ import TaskCard from '../components/task/TaskCard.vue'
 
 export default {
   components: { TaskCard },
-
+  data: () => ({
+    icons: {
+      numberIcon: 'mdi-clipboard-list-outline',
+      companyIcon: 'mdi-briefcase-variant-outline',
+      startIcon: 'mdi-archive-arrow-down-outline',
+      termIcon: 'mdi-briefcase-variant-outline',
+      workerIcon: 'mdi-account-circle-outline',
+      endIcon: 'mdi-alarm'
+    },
+    tasks: [
+      {
+        id: 1,
+        equipment: {
+          title: 'Тестовое оборудование',
+          description: 'Не работает какая-то херь'
+        },
+        data: [
+          {id: 1, title: 'O1230231', icon: 'mdi-clipboard-list-outline'},
+          {id: 2, title: 'ТоргТехникаСервис',icon: 'mdi-briefcase-variant-outline'},
+          {id: 3, status: 'Назначена'}
+        ],
+        timing: {
+          start: '11.12.2020 12:12',
+          term: '1 год до завершения',
+          worker: 'Пупкин П.П.',
+          end: '11.12.2020 12:12'
+        }
+      },
+      {
+        id: 2,
+        equipment: {
+          title: 'Новое оборудование',
+          description: 'Не работает что-то'
+        },
+        data: [
+          {id: 1, title: 'O1230231', icon: 'mdi-clipboard-list-outline'},
+          {id: 2, title: 'ТоргТехникаСервис',icon: 'mdi-briefcase-variant-outline'},
+          {id: 3, status: 'Назначена'}
+        ],
+        timing: {
+          start: '11.12.2020 12:12',
+          term: '1 год до завершения',
+          worker: 'Пупкин П.П.',
+          end: '11.12.2020 12:12'
+        }
+      },
+    ]
+  })
 }
 </script>
 
